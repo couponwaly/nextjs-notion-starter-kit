b71c0f2195b363c785b9e1c3187f8fba7f465f67
couponwaly <95749667+couponwaly@users.noreply.github.com>
2021-12-08 14:12:53 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 forever gulf</div>
+      <div className={styles.copyright}>Copyright 2021 couponwaly.com</div>
 
       {hasMounted ? (
         <div className={styles.settings}>
